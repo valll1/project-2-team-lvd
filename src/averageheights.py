6cd8c8387cd2eac64167605ecb9ccdee05b47d80
valeria ortega preciado <valeria.preciado.09@gmail.com>
2025-05-04 18:51:04 -0400

    Average height function

--- src/averageheights.py
+++ src/averageheights.py
@@ -1,1 +1,4 @@
-
+''' This function will find the average height in a given data frame'''
+def avg_height(frame): # Defines the function that will take the frames
+  avg_height = round(frame['Height'].sum() * 1.0 / len(frame['Height']),2) # Finds the avg of heights (in inches)
+  return avg_height # returns the average height of the data frame
